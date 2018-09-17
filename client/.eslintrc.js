b384f0cb2c1f4452d4d8bdf9de8c653a4aad234c
Joshua Docea <jddocea@gmail.com>
2018-09-17 09:34:57 -0500

    prettify html

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
     "extends": ["standard", "plugin:angular/johnpapa"],
-    "plugins": ["angular"],
+    "plugins": ["angular", "html"],
     "globals": {
       "angular": true
+    },
+    "settings": {
+      "html/html-extensions": [".html"],
+      "html/report-bad-indent": "warn",
     }
 };
