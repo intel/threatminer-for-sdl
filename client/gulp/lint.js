53de08104d0bea19b9d2149b85c152eebb495598
Joshua Docea <jddocea@gmail.com>
2018-09-17 21:40:53 -0500

    syntax changes

--- client/gulp/lint.js
+++ client/gulp/lint.js
@@ -3,26 +3,24 @@
 const gulp = require('gulp');
 const gulpIf = require('gulp-if');
 
-const plugins = {
-  eslint: require('gulp-eslint'),
-};
+const eslint = require('gulp-eslint');
 
 const files = require('./config').files;
 
 function isFixed(file) {
-	return file.plugins.eslint != null && file.plugins.eslint.fixed;
+	return file.eslint != null && file.eslint.fixed;
 }
 
 function lint() {
   // Note: To have the process exit with an error code (1) on lint error, return the stream and pipe to failOnError last.
   return gulp.src([files.moduleScripts, files.scripts, `!${files.tests}`, `!${files.bowerComponents}`, `!${files.tmp}`])
-    .pipe(plugins.eslint({
-      configFile: ".eslintrc.js" //,
-      //fix: true
+    .pipe(eslint({
+      configFile: ".eslintrc.js",
+      //fix: true,
     }))
-    .pipe(plugins.eslint.format())
-    //.pipe(gulp.dest('../test/fixtures'))
-    .pipe(plugins.eslint.failAfterError())
+    .pipe(eslint.format())
+    //.pipe(gulpIf(isFixed, gulp.dest('../test/fixtures')))
+    .pipe(eslint.failAfterError())
 }
 lint.description = 'Ensures scripts (.js files in app directory) follow the style standards set in the .eslintrc file';
 
@@ -30,9 +28,9 @@ function cilint() {
   const reporter = require('../eslint/reports/teamcity-lite-reporter');
   // Note: To have the process exit with an error code (1) on lint error, return the stream and pipe to failOnError last.
   return gulp.src([files.moduleScripts, files.scripts, `!${files.tests}`, `!${files.bowerComponents}`, `!${files.tmp}`])
-    .pipe(plugins.eslint({ rulePaths: ['eslint/rules/'] }))
-    .pipe(plugins.eslint.format(reporter, (results) => { }))
-    //.pipe(plugins.eslint.failAfterError());
+    .pipe(eslint({ rulePaths: ['eslint/rules/'] }))
+    .pipe(eslint.format(reporter, (results) => { }))
+    //.pipe(eslint.failAfterError());
 }
 cilint.description = 'Ensures scripts (.js files in app directory) follow the style standards set in the .eslintrc file';
 
